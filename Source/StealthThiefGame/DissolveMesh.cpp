af38156ae5e2ec7b2c5d764886929a6e001683ec
boxS0225 <ryota0225s@gmail.com>
2023-10-06 02:19:31 +0900

    消えるマテリアルをアクターコンポーネントに変更

---
+++ Source/StealthThiefGame/DissolveMesh.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DissolveMesh.h"
+#include "StealthThiefGameGameMode.h"
+
+// Sets default values for this component's properties
+UDissolveMesh::UDissolveMesh()
+{
+}
+
+UMaterialInstanceDynamic* UDissolveMesh::CreateDissolveMesh(UPrimitiveComponent* _mesh, int _elementIndex)
+{
+	auto skMesh = Cast<USkeletalMeshComponent>(_mesh);
+
+	//スケルタルメッシュをセット
+	if (skMesh != nullptr)
+	{
+		auto material = AddDissolveMesh(_mesh->CreateDynamicMaterialInstance(_elementIndex));
+		return material;
+	}
+
+	auto stMesh = Cast<UStaticMeshComponent>(_mesh);
+	//スタティックメッシュをセット
+	if (skMesh != nullptr)
+	{
+		auto material = AddDissolveMesh(_mesh->CreateDynamicMaterialInstance(_elementIndex));
+		return material;
+	}
+
+	return nullptr;
+}
+
+void UDissolveMesh::SetMaterialParameter(UMaterialInstanceDynamic* _target, FName _paramName, float _value)
+{
+	auto mid = GetDissolveMesh(_target);
+	//Nullチェック
+	AStealthThiefGameGameMode::CheckPointerContent<UMaterialInstanceDynamic>(mid);
+	//パラメータセット
+	mid->SetScalarParameterValue(_paramName, _value);
+}
---
+++ Source/StealthThiefGame/DissolveMesh.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Materials/MaterialInstanceDynamic.h"
+#include "DissolveMesh.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class STEALTHTHIEFGAME_API UDissolveMesh : public UActorComponent
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, Category = mesh)
+	TArray<UMaterialInstanceDynamic*> dissolveMeshs;
+
+public:	
+	// Sets default values for this component's properties
+	UDissolveMesh();
+
+protected:
+
+public:	
+	//マテリアルインスタンスの作成
+	UFUNCTION(BlueprintCallable)
+	UMaterialInstanceDynamic* CreateDissolveMesh(UPrimitiveComponent* _mesh, int _elementIndex);
+
+	//マテリアルインスタンスの数値を変更
+	UFUNCTION(BlueprintCallable)
+	void SetMaterialParameter(UMaterialInstanceDynamic* _target, FName _paramName, float _value);
+
+	FORCEINLINE TObjectPtr<UMaterialInstanceDynamic> AddDissolveMesh(const TObjectPtr<UMaterialInstanceDynamic> _mid) { dissolveMeshs.Add(_mid); return _mid; }
+	FORCEINLINE TObjectPtr<UMaterialInstanceDynamic> GetDissolveMesh(TObjectPtr<UMaterialInstanceDynamic> _target) const { int num = dissolveMeshs.Find(_target); return dissolveMeshs[num]; }
+};
